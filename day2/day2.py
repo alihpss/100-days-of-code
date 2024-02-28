cdefa8368d4343a1e008740ec100dee98ba620a2
alihpss <ali.hps@hotmail.com>
2024-02-28 18:20:18 -0300

    :sparkles: Feat: day 2

---
+++ day2/day2.py
@@ -0,0 +1,16 @@
+n1 = int(input('Insira um numero: '))
+n2 = int(input('Insira mais um numero: '))
+
+s = n1 + n2
+
+# print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+
+n = int(input('Digite um valor: '))
+n3 = int(input('Digite um valor: '))
+
+print('potencia de {} é: {}'.format(n3, n3**2))
+
+# print(type(n))
+
+#print(n)
+#print(n3.isnumeric())
