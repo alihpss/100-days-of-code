28bd8545f081100e0d6ed7833511cb23b31d08c8
alihpss <ali.hps@hotmail.com>
2024-03-01 09:49:03 -0300

    :sparkles: Feat: Day 3

--- day2/metros.py
+++ day2/metros.py
@@ -1,5 +1,5 @@
 metros = int(input('Digite os metros: '))
 centimetros = metros * 100
-milimetros = centimetros * 100
+milimetros = metros * 1000
 
 print('O valor de {} metros tem {} centimetros e {} milimetros'.format(metros, centimetros, milimetros))
